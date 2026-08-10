a15068630d17d37fab4bf0d89ec9a8e537bca56c
Jack Thomas <jack@jackthomas.io>
2026-08-09 22:54:37 -0500

    set nix profile path

--- script.sh
+++ script.sh
@@ -98,6 +98,7 @@ elif [[ "${OS_CLASS}" == "linux" ]]; then
             nebula
     elif [[ "${OS_DISTRO}" == "nixos" ]]; then
         nix --extra-experimental-features 'nix-command flakes' profile add \
+            --profile "${HOME}/profile" \
             nixpkgs#bitwarden-cli \
             nixpkgs#git \
             nixpkgs#nebula
